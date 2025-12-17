9eb4813e947879412afc1ce000d1b324abd186d3
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2025-12-17 15:47:41 -0500

    new: day 4 pt 2

--- day4/solution.go
+++ day4/solution.go
@@ -7,40 +7,55 @@ import (
 )
 
 func SolutionDay4(scnr *bufio.Scanner) {
+	symbols := "abcdefghijklmnaopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%^&*()-_=+,<.>;:'[{]}"
 	res := 0
 
-	var rollMap []string
+	var rollMap [][]byte
 	for scnr.Scan() {
-		rollMap = append(rollMap, scnr.Text())
+		rollMap = append(rollMap, []byte(scnr.Text()))
 	}
 
-	for r := 0; r < len(rollMap); r++ {
-		for c := 0; c < len(rollMap[r]); c++ {
-			if rollMap[r][c] != '@' {
-				continue
-			}
+	for lvl := 0; lvl < len(symbols); lvl++ {
+		gained := 0
 
-			// count paper rolls in all 8 adjacent spots
-			count := 0
-			for dr := -1; dr < 2; dr++ {
-				for dc := -1; dc < 2; dc++ {
-					nr, nc := r+dr, c+dc
-					if dr == 0 && dc == 0 { // looking at ourselves
-						continue
-					}
+		for r := 0; r < len(rollMap); r++ {
+			for c := 0; c < len(rollMap[r]); c++ {
+				if rollMap[r][c] != '@' {
+					continue
+				}
 
-					if nr >= 0 && nr < len(rollMap) && nc >= 0 && nc < len(rollMap[r]) {
-						if rollMap[nr][nc] == '@' {
-							count += 1
+				// count paper rolls in all 8 adjacent spots
+				count := 0
+				for dr := -1; dr < 2; dr++ {
+					for dc := -1; dc < 2; dc++ {
+						nr, nc := r+dr, c+dc
+						if dr == 0 && dc == 0 { // looking at ourselves
+							continue
+						}
+
+						if nr >= 0 && nr < len(rollMap) && nc >= 0 && nc < len(rollMap[r]) {
+							if rollMap[nr][nc] == '@' || rollMap[nr][nc] == symbols[lvl] {
+								count += 1
+							}
 						}
 					}
 				}
-			}
 
-			if count < 4 {
-				fmt.Println(r, c)
-				res += 1
+				if count < 4 {
+					rollMap[r][c] = symbols[lvl]
+					gained += 1
+				}
 			}
+		}
+
+		res += gained
+		if gained == 0 {
+			break
+		}
+
+		if lvl == len(symbols)-1 {
+			// TODO: "clean up" the graph by removing all non-@'s instead of giving up
+			fmt.Println("Ran out of symbols. Map may be unfinished.")
 		}
 	}
 
