8573309bbfdf7fd7365bbeed364bb7938a4c7f12
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2025-12-18 15:32:52 -0500

    new: day 5 pt 2

--- day5/solution.go
+++ day5/solution.go
@@ -30,69 +30,46 @@ func merge(slice []Interval) []Interval {
 	return res
 }
 
-func binarySearch(in []Interval, query int) bool {
-	l, r := 0, len(in)-1
-	for l <= r {
-		mid := (l + r) / 2
-		if in[mid].start > query {
-			r = mid - 1
-		} else if in[mid].end < query {
-			l = mid + 1
-		} else {
-			return true
-		}
+func count(slice []Interval) int {
+	res := 0
+	for _, interval := range slice {
+		res += (interval.end + 1) - interval.start
 	}
-
-	return false
+	return res
 }
 
 func SolutionDay5(scnr *bufio.Scanner) {
-	res := 0
 	var intervals []Interval
 
-	inRanges := true
 	for scnr.Scan() {
 		line := scnr.Text()
 		if line == "" {
-			inRanges = false
 			// sort ranges we have
 			sort.Slice(intervals, func(i, j int) bool {
 				return intervals[i].start < intervals[j].start
 			})
 			intervals = merge(intervals)
-			continue
+			break
 		}
 
-		if inRanges {
-			startStr, endStr, found := strings.Cut(line, "-")
-			if !found {
-				fmt.Println("Split not found in ranges?")
-				break
-			}
-
-			start, err := strconv.Atoi(startStr)
-			if err != nil {
-				fmt.Println("Failed to convert start to int", startStr)
-			}
-
-			end, err := strconv.Atoi(endStr)
-			if err != nil {
-				fmt.Println("Failed to convert start to int", endStr)
-			}
-
-			intervals = append(intervals, Interval{start, end})
-		} else {
-			query, err := strconv.Atoi(line)
-			if err != nil {
-				fmt.Println("Failed to convert query", line)
-				break
-			}
-
-			if binarySearch(intervals, query) {
-				res += 1
-			}
+		startStr, endStr, found := strings.Cut(line, "-")
+		if !found {
+			fmt.Println("Split not found in ranges?")
+			break
 		}
+
+		start, err := strconv.Atoi(startStr)
+		if err != nil {
+			fmt.Println("Failed to convert start to int", startStr)
+		}
+
+		end, err := strconv.Atoi(endStr)
+		if err != nil {
+			fmt.Println("Failed to convert start to int", endStr)
+		}
+
+		intervals = append(intervals, Interval{start, end})
 	}
 
-	fmt.Println(res)
+	fmt.Println(count(intervals))
 }
